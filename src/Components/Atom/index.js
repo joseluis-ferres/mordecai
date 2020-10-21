5ebcd692615ef8d2ae7532b314e2e85e3f4a0368
Jose Luis <joselferres@gmail.com>
2020-10-21 20:41:36 +0200

    Change folder

---
+++ src/Components/Atom/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../../src/logo.svg';
+import './styles.css';
+
+function Atom () {
+    return (
+        <img src={logo} className="Atom" alt="logo" />
+    );
+}
+
+export default Atom;
