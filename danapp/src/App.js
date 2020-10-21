c72ac0902515fc6c69821335c90240a2c2c26dbc
Jose Luis <joselferres@gmail.com>
2020-10-21 19:04:27 +0200

    Create component Logo

--- danapp/src/App.js
+++ danapp/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import Atom from './Components/Logo';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Atom />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Manu
         </a>
       </header>
     </div>
---
+++ danapp/src/Components/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../../src/logo.svg';
+
+function Atom () {
+    return (
+        <img src={logo} className="App-logo" alt="logo" />
+    );
+}
+
+export default Atom;
